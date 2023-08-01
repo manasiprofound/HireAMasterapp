0b3c69bcb12a01337bc73b019ceb26fdb4113996
manasiprofound <manasi.j@itprofound.com>
2023-08-01 12:27:47 -0500

    Admin module

---
+++ src/app/adminsubscriber/adminsubscriber.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminsubscriber',
+  templateUrl: './adminsubscriber.component.html',
+  styleUrls: ['./adminsubscriber.component.scss']
+})
+export class AdminsubscriberComponent {
+
+}
